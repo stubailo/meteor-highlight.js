459bda9aba5a876865e83d1fb2d872e3675a5b40
Sashko Stubailo <sashko@stubailo.com>
2015-10-06 22:36:46 -0700

    Don't highlight code when we don't have the language

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Code highlighting integrated with the markdown package",
-  version: "1.1.0",
+  version: "1.2.0",
   name: "simple:highlight.js",
   git: "https://github.com/stubailo/meteor-highlight.js"
 });
